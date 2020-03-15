6c7920b6e2f1caa35b677179c34e83dc6278362a
Ayoub150 <54031374+Ayoub150@users.noreply.github.com>
2020-03-15 18:40:56 +0300

    Update Ssss.py

--- Ssss.py
+++ Ssss.py
@@ -221,6 +221,56 @@ def lookup(index, keyword):
     else:
         return None
 
+# remove tags 
+
+# Question 4: Remove Tags
+
+# When we add our words to the index, we don't really want to include
+# html tags such as <body>, <head>, <table>, <a href="..."> and so on.
+
+# Write a procedure, remove_tags, that takes as input a string and returns
+# a list of words, in order, with the tags removed. Tags are defined to be
+# strings surrounded by < >. Words are separated by whitespace or tags. 
+# You may assume the input does not include any unclosed tags, that is,  
+# there will be no '<' without a following '>'.
+
+def remove_tags(s):
+    checker = True
+    result = []
+    word = ""
+    for e in s:
+        if e == '<':
+            if word:
+                result = result + word.split()
+                word = ""
+            checker = False
+        if checker:
+            word = word + e
+        if e == '>':
+            checker = True
+    if word:
+        result = result + word.split()
+    return result
+
+
+
+#print remove_tags('''<h1>Title</h1><p>This is a
+                    <a href="http://www.udacity.com">link</a>.<p>''')
+#>>> ['Title','This','is','a','link','.']
+
+#print remove_tags('''<table cellpadding='3'>
+                     <tr><td>Hello</td><td>World!</td></tr>
+                     </table>''')
+#>>> ['Hello','World!']
+
+#print remove_tags("<hello><goodbye>")
+#>>> []
+
+#print remove_tags("This is plain text.")
+#>>> ['This', 'is', 'plain', 'text.']
+
+
+
 def crawl_web(seed): # returns index, graph of inlinks
     tocrawl = [seed]
     crawled = []
